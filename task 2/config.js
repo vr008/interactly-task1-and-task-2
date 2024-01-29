08191706b3f62c0ebe93405ae9a3d69a093c287d
vr008 <114496315+vr008@users.noreply.github.com>
2024-01-29 20:53:15 +0530

    Add files via upload

---
+++ task 2/config.js
@@ -0,0 +1,8 @@
+/*module.exports = {
+  contact: 'contactlist.txt',
+  content:'/your-video.mp4'
+}*/
+module.exports = {
+  contact: './contactlist.txt', // Replace with the path to your contact list file
+  videoPath: './your-video.mp4', // Replace with the path to your video file
+};
